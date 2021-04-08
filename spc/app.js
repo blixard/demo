b1d64189cef1f83c05884047e65ffc2658b77be5
Shubha <rupelpadhy@gmail.com>
2021-04-08 16:35:52 +0530

    testing

--- spc/app.js
+++ spc/app.js
@@ -1,6 +1,7 @@
 // gsap.to(".box", {x: 100, duration: 1});
 
 window.addEventListener("DOMContentLoaded", (e)=>{
+    animate2()
     animate()
 })
 
@@ -10,18 +11,48 @@ animate = ()=>{
             duration:2,
             trigger:".box",
             markers:true,
-            start:"top 70%",
-            end:"top 0%",
+            start:"top 50%",
+            end:"top 30%",
             scrub:1,
         },
     })
     tl.to(".box",{
-        x:500,
+        x:50,
         rotation:360,
-        duration:3
+        duration:3,
+        ease:"power4"
     })
     tl.to(".box",{
         duration:5,
-        y:500
+        y:100,
+        ease:"power4"
+    })
+}
+
+animate2 = ()=>{
+    var tl = gsap.timeline({
+        scrollTrigger:{
+            duration:2,
+            trigger:".page1",
+            markers:true,
+            start:"top 0%",
+            end:"top 30%",
+            scrub:1,
+        },
+    })
+    tl.to(".page1",{
+        rotation:30,
+        duration:3,
+        // ease:"power4"
+    })
+    // tl.to(".page1",{
+    //     background:"black",
+    //     duration:1,
+    //     ease:"elastic"
+    // })
+    tl.to(".page1",{
+        duration:5,
+        y:100,
+        ease:"power4"
     })
 }
