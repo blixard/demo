5687fbfec2c710fff9fd7e4b573eec4e1e01449f
Shubha <rupelpadhy@gmail.com>
2021-04-12 22:36:26 +0530

    some changed and a 2nd page ?

--- spc/app.js
+++ spc/app.js
@@ -1,9 +1,8 @@
 // gsap.to(".box", {x: 100, duration: 1});
 
 window.addEventListener("DOMContentLoaded", (e)=>{
-    // animatePage1()
+    animatePage1()
     animatePage2()
-    // animateT();
     console.log("hi")
     // animate()
 })
@@ -33,29 +32,6 @@ animate = ()=>{
 }
 
 animatePage1 = ()=>{
-    var tl = gsap.timeline({
-        scrollTrigger:{
-            duration:2,
-            trigger:".page1",
-            markers:true,
-            start:"center",
-            end:"+=500",
-            scrub:1,
-        },
-    })
-    tl.to(".page1",{
-        rotation:30,
-        duration:3,
-        // ease:"power4"
-    })
-    tl.to(".page1",{
-        duration:5,
-        y:100,
-        ease:"power4"
-    })
-}
-
-animatePage2 = ()=>{
     var tl = gsap.timeline({
         scrollTrigger:{
             duration:2,
@@ -109,39 +85,63 @@ animatePage2 = ()=>{
         yPercent:-300,
         xPercent:-400
     })
+
 }
 
-// animateT = ()=>{
-//     var tl = gsap.timeline()
-//     tl.from(".img1",{
-//         xPercent:-100
-//     })
-//     tl.from(".img2",{
-//         xPercent:-200
-//     })
-//     tl.from(".img3",{
-//         xPercent:-300
-//     })
-//     tl.from(".img4",{
-//         xPercent:-400
-//     })
+animatePage2 = ()=>{
+    var tl = gsap.timeline({
+        scrollTrigger:{
+            duration:2,
+            trigger:".page2",
+            markers:true,
+            start:"top",
+            end:"+=2000",
+            scrub:1,
+            pin: true,
+        },
+    })
+    tl.from(".p2img1",{
+        xPercent:-100
+    })
+    tl.from(".p2img2",{
+        yPercent:-100,
+        xPercent:-200
+    })
+    tl.from(".p2img3",{
+        xPercent:-300
+    })
+    tl.from(".p2img4",{
+        yPercent:-100,
+        xPercent:-400
+    })
+    tl.from(".p2img5",{
+        xPercent:-100
+    })
+    tl.from(".p2img6",{
+        yPercent:-200,
+        xPercent:-200
+    })
+    tl.from(".p2img7",{
+        xPercent:-300
+    })
+    tl.from(".p2img8",{
+        yPercent:-200,
+        xPercent:-400
+    })
+    tl.from(".p2img9",{
+        xPercent:-100
+    })
+    tl.from(".p2img10",{
+        yPercent:-300,
+        xPercent:-200
+    })
+    tl.from(".p2img11",{
+        xPercent:-300
+    })
+    tl.from(".p2img12",{
+        yPercent:-300,
+        xPercent:-400
+    })
+    
 
-// }
-
-// function fun(){
-//         var tl = gsap.timeline()
-//         tl.from(".img1",{
-//             xPercent:-100
-//         })
-//         tl.from(".img2",{
-//             xPercent:-200
-//         })
-//         tl.from(".img3",{
-//             xPercent:-300
-//         })
-//         tl.from(".img4",{
-//             xPercent:-400
-//         })
-
-//         tl.reverse();
-// }
+}
