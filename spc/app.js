067e659f09e62ba1ec40468fd46063003b5bfa3a
Shubha <rupelpadhy@gmail.com>
2021-04-13 21:20:41 +0530

    a lot of changes made

--- spc/app.js
+++ spc/app.js
@@ -3,7 +3,11 @@
 window.addEventListener("DOMContentLoaded", (e)=>{
     animatePage1()
     animatePage2()
-    console.log("hi")
+    var x = document.querySelectorAll(".list a")
+    x[0].onClick = function(){
+        console.log("hello")
+    }
+    console.log(x[0]);
 })
 
 animatePage1 = ()=>{
@@ -31,6 +35,40 @@ animatePage1 = ()=>{
     tl.from(".img4",{
         yPercent:-100,
         xPercent:-400
+    })
+    tl.from(".img5",{
+        xPercent:-100
+    })
+    tl.from(".img6",{
+        yPercent:-100,
+        xPercent:-200
+    })
+    tl.from(".img7",{
+        xPercent:-300
+    })
+    tl.from(".img8",{
+        yPercent:-100,
+        xPercent:-400
+    })
+    tl.from(".mid",{
+        xPercent:-100
+    })
+    tl.from(".img9",{
+        xPercent:-100
+    })
+    tl.from(".img10",{
+        yPercent:-100,
+        xPercent:-200
+    })
+    tl.from(".img11",{
+        xPercent:-300
+    })
+    tl.from(".img12",{
+        yPercent:-100,
+        xPercent:-400
+    })
+    tl.from(".bottom",{
+        xPercent:-100
     })
 }
 
@@ -70,4 +108,17 @@ animatePage2 = ()=>{
         xPercent:-2500
     }) 
 
+}
+
+var flag = false;
+function fun(){
+    console.log("working button")
+    if(flag == false){
+        document.querySelector(".menu").style.left=0;
+        flag = true;
+    }
+    else if(flag == true){
+        document.querySelector(".menu").style.left="-100%";
+        flag = false;
+    }
 }
